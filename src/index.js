8a4d7062590cccd326fe7a87a5c81d2120cc3d13
Matt Grabowski <m-grabowski@kellogg.northwestern.edu>
2017-03-08 19:59:08 -0600

    Ch1

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ var NoCurrentMovie = require('./components/NoCurrentMovie')
 var SortBar = require('./components/SortBar')
 
 // There should really be some JSON-formatted data in movies.json, instead of an empty array.
-// I started writing this command to extract the data from the learn-sql workspace 
+// I started writing this command to extract the data from the learn-sql workspace
 // on C9, but it's not done yet :) You must have the csvtojson command installed on your
 // C9 workspace for this to work.
 // npm install -g csvtojson
@@ -27,8 +27,8 @@ var SortBar = require('./components/SortBar')
 // Firebase configuration
 var Rebase = require('re-base')
 var base = Rebase.createClass({
-  apiKey: "...",   // replace with your Firebase application's API key
-  databaseURL: "...", // replace with your Firebase application's database URL
+  apiKey: "AIzaSyAzzJAUc6NTXSi3l7kPsnDHkpHdrFUEums",   // replace with your Firebase application's API key
+  databaseURL: "https://final-97a0d.firebaseio.com/", // replace with your Firebase application's database URL
 })
 
 var App = React.createClass({
